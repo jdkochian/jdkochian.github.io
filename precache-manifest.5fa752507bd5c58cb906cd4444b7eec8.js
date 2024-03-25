716818dea2ceb0bc22623f738eb64bdfa0da7ec3
jdkochian <jdkochian@gmail.com>
2024-03-25 17:30:04 -0400

    Updates

---
+++ precache-manifest.5fa752507bd5c58cb906cd4444b7eec8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7522a84731430a7d11e2794a9a7ba62f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba85fda1d82055c2fbe5",
+    "url": "/static/css/main.987dd690.chunk.css"
+  },
+  {
+    "revision": "b05401927ad0a1a684bb",
+    "url": "/static/js/2.ff25e691.chunk.js"
+  },
+  {
+    "revision": "12adf5ee6e135646e379",
+    "url": "/static/js/3.8ba39429.chunk.js"
+  },
+  {
+    "revision": "ba85fda1d82055c2fbe5",
+    "url": "/static/js/main.12006441.chunk.js"
+  },
+  {
+    "revision": "49b1760ced1044a52545",
+    "url": "/static/js/runtime~main.e2e3066b.js"
+  },
+  {
+    "revision": "879781e8a99890c3d27745f60b8971f9",
+    "url": "/static/media/Resume.879781e8.pdf"
+  },
+  {
+    "revision": "7a2e7622362e74fc2bb93cabb8da32fb",
+    "url": "/static/media/email.7a2e7622.svg"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/static/media/github.1bb02710.svg"
+  },
+  {
+    "revision": "bdf07139f1006b5f50826a7d120738db",
+    "url": "/static/media/headshot.bdf07139.JPG"
+  },
+  {
+    "revision": "9a4f74ce1edc1a55646cbc9a9343fb5a",
+    "url": "/static/media/linkedin.9a4f74ce.svg"
+  },
+  {
+    "revision": "2144889e8e7408c4b9bf90ae2c43b09c",
+    "url": "/static/media/phone.2144889e.svg"
+  }
+]);
